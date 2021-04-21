61d48423e0110a0bb772b1bb700e520975991970
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-21 10:28:23 +0200

    istalling heroicons and grabbing 2 icons

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import Avatar from '../components/Avatar'
+import Head from 'next/head';
+import Avatar from '../components/Avatar';
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
 
 export default function Home() {
   return (
@@ -21,8 +22,8 @@ export default function Home() {
                 <p className="link">Images</p>
                   
                   {/* Icon */}
+                  <ViewGridIcon />
                   
-                  {/* Avatar */}
                   <Avatar url="https://scontent.fgse1-1.fna.fbcdn.net/v/t1.6435-9/103695651_3163119120436269_5701807603451754723_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=a40Yd2corEIAX8lHb0O&_nc_ht=scontent.fgse1-1.fna&oh=960f329ae324006df2672d03b3b9e843&oe=60A73183" />
 
               </div>
