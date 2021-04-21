7dbbe44276af6bdcc99d3db97fb24689a6b5b60a
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-21 10:21:32 +0200

    avatar component

---
+++ components/Avatar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Avatar = ({url}) => {
+    return (
+        <img
+            loading="lazy"
+            className="rounded-full h-10 cursor-pointer transition duration-150 transform hover:scale-110"
+            src={url}
+            alt="profile pic"
+        />
+    );
+}
+
+export default Avatar;
