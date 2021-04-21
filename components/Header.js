48d3908030678eb6d48b986b22fe886be83f1664
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-21 14:21:26 +0200

    starting search page and header

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Header = () => {
+    return (
+        <header>
+            <Image
+                src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+                height={40}
+                width={120}
+                className="cursor-pointer"
+            />
+        </header>
+    );
+}
+
+export default Header;
---
+++ pages/search.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import React from 'react';
+import Header from '../components/Header';
+
+const Search = () => {
+    return (
+        <div>
+            <Head>
+                <title>Search Results</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+
+            {/* Header */}
+            <Header />
+
+            {/* Search Results */}
+        </div>
+    );
+}
+
+export default Search;
