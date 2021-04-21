020426ebf8d98ed3c88badce13f83191a49ee8d9
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-21 11:26:42 +0200

    styling search component

--- pages/index.js
+++ pages/index.js
@@ -30,13 +30,13 @@ export default function Home() {
           </header>
 
           {/* Body */}
-          <form action="">
+          <form className="flex flex-col items-center mt-44 flex-grow">
               <Image
                   src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
                   height={100}
                   width={300}
               />
-              <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md " >
+              <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl" >
                   <SearchIcon className="h-5 mr-3 text-gray-500" />
                   <input type="text" className="flex-grow focus:outline-none" />
                   <MicrophoneIcon className="h-5" />
