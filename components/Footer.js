07713c8f22e6a0186aced305a11f50450276a1d4
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-21 13:00:50 +0200

    adding content to footer and some styling

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,24 @@ import { GlobeIcon } from '@heroicons/react/solid';
 
 const Footer = () => {
     return (
-        <footer>
-            <div>
-                
+        <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
+            <div className="px-8 py-3">
+                <p>Sweden</p>
+            </div>
+            <div className=" grid grid-cols-1">
+                <div>
+                    <GlobeIcon className="h-5 mr-1 text-green-700" /> Carbon neutral since 2007
+                </div>
+                <div>
+                    <p>Advertising</p>
+                    <p>Business</p>
+                    <p>How Search works</p>
+                </div>
+                <div>
+                    <p>Privacy</p>
+                    <p>Terms</p>
+                    <p>Settings</p>
+                </div>
             </div>
         </footer>
     );
