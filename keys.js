f126fc4e63f344e3c121fc7c0ddaa28c4ef17c12
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-21 17:57:33 +0200

    .env setup - securing api_key

--- keys.js
+++ keys.js
@@ -1,5 +1,7 @@
-// PUT IN .ENV
+export const API_KEY = process.env.API_KEY;
+export const CONTEXT_KEY = process.env.CONTEXT_KEY;
 
-// API Key - https://developers.google.com/custom-search/v1/using_rest
 
 // Context Key - https://cse.google.com/cse/create/new
+
+// API Key - https://developers.google.com/custom-search/v1/using_rest
