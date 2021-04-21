b8661aa13dff118bb02e6be0c5bdc58890eaebab
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-21 12:50:33 +0200

    layout big fix and starting footer

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { GlobeIcon } from '@heroicons/react/solid';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
