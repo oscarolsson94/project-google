26e78f03f3408b233ee7c63a9c8ff95e76ccf5fa
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-21 10:31:14 +0200

    styling grid icon

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,9 @@ export default function Home() {
                 <p className="link">Gmail</p>
                 <p className="link">Images</p>
                   
-                  {/* Icon */}
-                  <ViewGridIcon />
+                <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
                   
-                  <Avatar url="https://scontent.fgse1-1.fna.fbcdn.net/v/t1.6435-9/103695651_3163119120436269_5701807603451754723_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=a40Yd2corEIAX8lHb0O&_nc_ht=scontent.fgse1-1.fna&oh=960f329ae324006df2672d03b3b9e843&oe=60A73183" />
+                <Avatar url="https://scontent.fgse1-1.fna.fbcdn.net/v/t1.6435-9/103695651_3163119120436269_5701807603451754723_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=a40Yd2corEIAX8lHb0O&_nc_ht=scontent.fgse1-1.fna&oh=960f329ae324006df2672d03b3b9e843&oe=60A73183" />
 
               </div>
           </header>
