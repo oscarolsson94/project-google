e07c95bb8e6784d3f49b8289904f2a4af2d984d5
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-21 11:06:17 +0200

    adding config file to be able to use next.js image

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Avatar from '../components/Avatar';
 import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -24,11 +25,17 @@ export default function Home() {
                 <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
                   
                 <Avatar url="https://scontent.fgse1-1.fna.fbcdn.net/v/t1.6435-9/103695651_3163119120436269_5701807603451754723_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=a40Yd2corEIAX8lHb0O&_nc_ht=scontent.fgse1-1.fna&oh=960f329ae324006df2672d03b3b9e843&oe=60A73183" />
-
               </div>
           </header>
 
           {/* Body */}
+          <form action="">
+              <Image
+                  src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+                  height={100}
+                  width={300}
+              />
+          </form>
 
           {/* Footer */}
 
