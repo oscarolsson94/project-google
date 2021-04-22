91fe2c9af136b24fcdc1d4e32f5053ae17a2ded5
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-22 08:15:41 +0200

    setting up ssr

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import React from 'react';
 import Header from '../components/Header';
+import { API_KEY, CONTEXT_KEY } from '../keys';
 
-const Search = () => {
+const Search = ({results}) => {
     return (
         <div>
             <Head>
@@ -22,5 +23,13 @@ export default Search;
 export async function getServerSideProps(context) {
     const useDummyData = false;
 
+    const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${}&q=${context.query.term}`)
+        .then(response => response.json());
     
+    // After the server has rendered, pass the results to the client
+    return {
+        props: {
+            results: data,
+        }
+    }
 }
