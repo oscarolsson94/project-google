5d2c3e4b20ee34ae323148eb727ea354eb5bf093
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-22 08:20:57 +0200

    setting up a file for dummy data to no exhaust api

--- pages/search.js
+++ pages/search.js
@@ -3,7 +3,8 @@ import React from 'react';
 import Header from '../components/Header';
 import { API_KEY, CONTEXT_KEY } from '../keys';
 
-const Search = ({results}) => {
+const Search = ({ results }) => {
+    console.log(results);
     return (
         <div>
             <Head>
@@ -23,7 +24,7 @@ export default Search;
 export async function getServerSideProps(context) {
     const useDummyData = false;
 
-    const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${}&q=${context.query.term}`)
+    const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`)
         .then(response => response.json());
     
     // After the server has rendered, pass the results to the client
