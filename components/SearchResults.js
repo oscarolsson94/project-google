0ffe5ea59563851beb2586d813ced1734a5c80dd
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-22 08:43:27 +0200

    looping thru search results and outputting

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -6,6 +6,19 @@ const SearchResults = ({results}) => {
             <p className="text-gray-600 text-md mb-5 mt-3" >
                 About {results.searchInformation?.formattedTotalResults} results ({results.searchInformation?.formattedSearchTime} seconds)
             </p>
+
+            {results.items?.map((result) => (
+                <div key={result.link} className="max-w-xl mb-8" >
+                    <div>
+                        <a href={result.link} className="text-sm" >{result.formattedUrl}</a>
+                        <a href={result.link}>
+                            <h2 className="truncate text-xl text-blue-800 font-medium" >{ result.title }</h2>
+                        </a>
+                    </div>
+                    <p>{ result.snippet }</p>
+                </div>
+            ))}
+
         </div>
     );
 }
